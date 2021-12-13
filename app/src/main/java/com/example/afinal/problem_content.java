1aab51ae3dd21f755582b78b93659d7bd949393c
s1081421 <91941516+s1081421@users.noreply.github.com>
2021-12-13 22:39:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/afinal/problem_content.java
@@ -0,0 +1,14 @@
+package com.example.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class problem_content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_problem_content);
+    }
+}
